a513776eaae3fce69382defb22a1d44f6bbeed7b
Ruchithathota29 <ruchitha.thota@cuw.edu>
2025-09-21 21:54:09 -0500

    Update Dijkstra's Algorithm.cpp

--- Dijkstra's Algorithm.cpp
+++ Dijkstra's Algorithm.cpp
@@ -7,10 +7,8 @@
 #include <string>
 
 using namespace std;
-
 const int INF = numeric_limits<int>::max();
 
-// directed graph
 unordered_map<string, vector<pair<string, int>>> directedGraph = {
     {"Dorm", {make_pair("Cafeteria", 5), make_pair("Library", 7)}},
     {"Cafeteria", {make_pair("LectureHall", 3)}},
@@ -20,78 +18,68 @@ unordered_map<string, vector<pair<string, int>>> directedGraph = {
     {"Gym", {}}
 };
 
-// Converts directed graph to undirected by adding reverse edges
 unordered_map<string, vector<pair<string, int>>> makeUndirectedGraph(
-    const unordered_map<string, vector<pair<string, int>>>& inputGraph) {
-    
-    unordered_map<string, vector<pair<string, int>>> undirectedGraph = inputGraph;
-
-    for (const auto& entry : inputGraph) {
-        const string& from = entry.first;
-        for (const auto& edge : entry.second) {
-            const string& to = edge.first;
-            int weight = edge.second;
-            undirectedGraph[to].push_back(make_pair(from, weight));
+    const unordered_map<string, vector<pair<string, int>>>& input) {
+    unordered_map<string, vector<pair<string, int>>> graph = input;
+    for (unordered_map<string, vector<pair<string, int>>>::const_iterator it = input.begin(); it != input.end(); ++it) {
+        const string& from = it->first;
+        const vector<pair<string, int>>& edges = it->second;
+        for (size_t i = 0; i < edges.size(); ++i) {
+            const string& to = edges[i].first;
+            int weight = edges[i].second;
+            graph[to].push_back(make_pair(from, weight));
         }
     }
-
-    return undirectedGraph;
+    return graph;
 }
 
 void findShortestPath(const string& start, const string& end,
                       const unordered_map<string, vector<pair<string, int>>>& graph) {
-    unordered_map<string, int> shortestCost;
-    unordered_map<string, string> cameFrom;
-    priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> nextStops;
+    unordered_map<string, int> cost;
+    unordered_map<string, string> prev;
+    priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> pq;
 
-    for (unordered_map<string, vector<pair<string, int>>>::const_iterator it = graph.begin(); it != graph.end(); ++it) {
-        shortestCost[it->first] = INF;
-    }
+    for (unordered_map<string, vector<pair<string, int>>>::const_iterator it = graph.begin(); it != graph.end(); ++it)
+        cost[it->first] = INF;
 
-    shortestCost[start] = 0;
-    nextStops.push(make_pair(0, start));
+    cost[start] = 0;
+    pq.push(make_pair(0, start));
 
-    while (!nextStops.empty()) {
-        pair<int, string> top = nextStops.top();
-        nextStops.pop();
-        int currentCost = top.first;
-        string currentLocation = top.second;
+    while (!pq.empty()) {
+        pair<int, string> top = pq.top(); pq.pop();
+        int currCost = top.first;
+        string curr = top.second;
+        if (currCost > cost[curr]) continue;
 
-        if (currentCost > shortestCost[currentLocation]) continue;
-
-        vector<pair<string, int>> neighbors = graph.at(currentLocation);
+        const vector<pair<string, int>>& neighbors = graph.at(curr);
         for (size_t i = 0; i < neighbors.size(); ++i) {
-            string neighbor = neighbors[i].first;
-            int travelTime = neighbors[i].second;
-            int newCost = currentCost + travelTime;
-
-            if (newCost < shortestCost[neighbor]) {
-                shortestCost[neighbor] = newCost;
-                cameFrom[neighbor] = currentLocation;
-                nextStops.push(make_pair(newCost, neighbor));
+            string next = neighbors[i].first;
+            int time = neighbors[i].second;
+            int newCost = currCost + time;
+            if (newCost < cost[next]) {
+                cost[next] = newCost;
+                prev[next] = curr;
+                pq.push(make_pair(newCost, next));
             }
         }
     }
 
-    if (shortestCost[end] == INF) {
+    if (cost[end] == INF) {
         cout << "No path found from " << start << " to " << end << endl;
         return;
     }
 
-    // Reconstruct path
     stack<string> path;
     string current = end;
     while (current != start) {
         path.push(current);
-        current = cameFrom[current];
+        current = prev[current];
     }
     path.push(start);
 
-    cout << "Shortest travel time: " << shortestCost[end] << " minutes" << endl;
-    cout << "Route: ";
+    cout << "Shortest travel time: " << cost[end] << " minutes\nRoute: ";
     while (!path.empty()) {
-        cout << path.top();
-        path.pop();
+        cout << path.top(); path.pop();
         if (!path.empty()) cout << " -> ";
     }
     cout << endl;
@@ -99,19 +87,14 @@ void findShortestPath(const string& start, const string& end,
 
 int main() {
     string start, end;
-    cout << "Enter starting location (e.g., Dorm): ";
-    cin >> start;
-    cout << "Enter destination (e.g., Gym): ";
-    cin >> end;
+    cout << "Enter starting location: "; cin >> start;
+    cout << "Enter destination: "; cin >> end;
 
-    // Convert to undirected graph
     unordered_map<string, vector<pair<string, int>>> graph = makeUndirectedGraph(directedGraph);
-
     if (graph.find(start) == graph.end() || graph.find(end) == graph.end()) {
         cout << "Invalid location. Available places: ";
-        for (unordered_map<string, vector<pair<string, int>>>::iterator it = graph.begin(); it != graph.end(); ++it) {
+        for (unordered_map<string, vector<pair<string, int>>>::iterator it = graph.begin(); it != graph.end(); ++it)
             cout << it->first << " ";
-        }
         cout << endl;
         return 1;
     }
